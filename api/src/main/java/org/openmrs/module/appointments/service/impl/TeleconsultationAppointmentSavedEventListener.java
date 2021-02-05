8c01309f6f0af665f90204c89245388b2edd8fd0
Mahesh-I <mahesh.ippili@thoughtworks.com>
2021-02-05 17:52:26 +0530

    include emailSent status in response of create appointment

--- api/src/main/java/org/openmrs/module/appointments/service/impl/TeleconsultationAppointmentSavedEventListener.java
+++ api/src/main/java/org/openmrs/module/appointments/service/impl/TeleconsultationAppointmentSavedEventListener.java
@@ -26,6 +26,7 @@ public class TeleconsultationAppointmentSavedEventListener implements Applicatio
     public void onApplicationEvent(TeleconsultationAppointmentSavedEvent event) {
         try {
             emailNotificationService.sendTeleconsultationAppointmentLinkEmail(event.getAppointment());
+            event.getAppointment().setEmailSent(true);
         } catch (EmailNotificationException e) {
             event.getAppointment().setEmailSent(false);
             log.error("Unable to send teleconsultation appointment email notification", e);
